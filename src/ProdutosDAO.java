ddea32f2444ef9a1a5c499b78ab6c18be141f85f
kevin2003-rgb <adriankevin390@gmail.com>
2026-05-24 19:56:01 -0300

    Listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -30,13 +30,42 @@ public class ProdutosDAO {
         
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
+    public ArrayList<ProdutosDTO> listarProdutos() {
 
+    String sql = "SELECT * FROM produtos";
+
+    try {
+
+        conn = new conectaDAO().connectDB();
+
+        prep = conn.prepareStatement(sql);
+
+        resultset = prep.executeQuery();
+
+        while(resultset.next()) {
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+
+        }
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos!");
+
+    }
+
+    return listagem;
+    }
+}
+    
+    
+    
+    
+    
